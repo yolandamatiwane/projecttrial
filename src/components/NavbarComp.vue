c60ed08f5f6b55ce62eae5785ebc4c393d3d46d1
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-06-27 16:17:03 +0200

    end of day

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,19 +1,15 @@
 <template>
     <nav>
         <img src="../assets/Beige Minimal Personal Make Up Artist Logo.png">
-        <router-link to="/">Home</router-link> 
-        <router-link to="/about"> About</router-link>
+        <router-link to="/"><span @mousemove="hehe=true" @mouseleave="hehe=false">Home</span></router-link> 
+        <router-link to="/about"><span @mousemove="hehe=true" @mouseleave="hehe=false">About</span></router-link>
         <router-link to="/resume"> Resume</router-link>
         <router-link to="/projects"> Projects</router-link>
         <router-link to="/testimonials"> Testimonials</router-link>
         <router-link to="/contacts"> Contacts</router-link>
   </nav>
-  <div 
-  @mouseover="hehe =true"
-  @mouseleave="hehe =false"
-  >hehe hover over me</div>
 
-  <p v-if="hehe">this is the power of hehe</p>
+  <span v-if="hehe">home</span>
 </template>
 <script>
 
@@ -21,7 +17,7 @@
 export default {
     data() {
         return {
-            home:false
+            hehe:false
         }
     },
 }
