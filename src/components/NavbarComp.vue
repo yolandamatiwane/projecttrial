f5aea3fc0d93f60be44bac75e7e828dd1f8201c0
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-06-27 23:45:10 -0700

    tried stuff

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,15 +1,15 @@
 <template>
     <nav>
         <img src="../assets/Beige Minimal Personal Make Up Artist Logo.png">
-        <router-link to="/"><span @mousemove="hehe=true" @mouseleave="hehe=false">Home</span></router-link> 
-        <router-link to="/about"><span @mousemove="hehe=true" @mouseleave="hehe=false">About</span></router-link>
-        <router-link to="/resume"> Resume</router-link>
-        <router-link to="/projects"> Projects</router-link>
-        <router-link to="/testimonials"> Testimonials</router-link>
-        <router-link to="/contacts"> Contacts</router-link>
+        <router-link to="/" @mousemove="show('Home')" @mouseleave="hide()">Home</router-link> 
+        <router-link to="/about" @mousemove="show('About)" @mouseleave="hide()">About</router-link>
+        <router-link to="/resume" @mousemove="show('Resume)" @mouseleave="hide()"> Resume</router-link>
+        <router-link to="/projects" @mousemove="show('Projects)" @mouseleave="hide()"> Projects</router-link>
+        <router-link to="/testimonials" @mousemove="show('Testimonials)" @mouseleave="hide()"> Testimonials</router-link>
+        <router-link to="/contacts" @mousemove="show('About)" @mouseleave="hide()"> Contacts</router-link>
   </nav>
 
-  <span v-if="hehe">home</span>
+  <span v-if="displayLabel" class="nav-label">{{ label }}</span>
 </template>
 <script>
 
@@ -17,9 +17,19 @@
 export default {
     data() {
         return {
-            hehe:false
+            displayLabel:false,
+            label: ''
         }
     },
+    methods:{
+        show(name){
+            this.displayLabel = true;
+            this.label = name;
+        },
+        hide(){
+            this.displayLabel = false;
+        }
+    }
 }
 </script>
 <style scoped>
